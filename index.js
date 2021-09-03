fd8d8c85888c4f7c7ab698e1a778f44f3fb6f38b
dilkushrajpurohit <dilkushpurohit963@gmail.com>
2021-09-03 01:55:34 -0700

    main

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ var path=require("path")
  app.get("/Login",(req,res)=>{
  	res.sendfile(__dirname+"/static/email.html")
  })
+ app.get("/social",(req,res)=>{
+  res.sendfile("static/social.html")
+ })
 app.get("/",(req,res)=>[
 	res.sendfile(__dirname +"/static/index.html")])
 app.listen(port,()=>{
