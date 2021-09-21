41e9f77871405e5a3d48c5741442592c88716540
dilkushrajpurohit <dilkushpurohit963@gmail.com>
2021-09-20 17:50:20 -0700

    main

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ var path=require("path")
 var crypto=require("crypto")
 var nodemailer=require("nodemailer")
 //-----------------Creating database and inserting------------
-const mongoose=require("mongoose");
-const mongodb=require("mongodb")
+
 //connectimng to database-------------------------------
 //mongoose.connect("mongodb://localhost:27017/factnotes",{useNewUrlParser:true}).then(()=>console.log("Successfully connected to database"))
  app.use(express.static(path.join(__dirname + "/static")));
@@ -26,69 +25,12 @@ const mongodb=require("mongodb")
     res.sendfile("static/social.html")
   }
  })
-///------------------------Creating endpoint for saving data******************
- const Registerschemea= new mongoose.Schema({
-    username:{
-      type:String,
-      required:"username is required"
-    },
-    email:{
-      type:String,
-      required:"Email is required",
-      unique:true
-    },
-    class:{
-      type:Number,
-      required:"number is required",
-
-    },
-    region:{
-      type:String,
-      required:"please Mention Region"
-    },
-    password:{
-      type:String,
-      required:"please enter password"
-    },
-    verified:{
-      type:Boolean,
-      default:false
-    },
-    emailtoken:String
-
-  })
+///------------------------Creating endpoint for saving data******************ntion Region"
+  
 
 
   ///////////////models///////
-  var factmodel=new mongoose.model("notesfact",Registerschemea)
-   const validate = (factmodel) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).max(255).required(),
-    email: Joi.string().email().required(),
-  });
-  return schema.validate(user);
-};
-app.get("/login_verify",(req,res)=>{
-  var login= req.query.email
-
-  const read=new factmodel({
-    username:`${req.query.username}`,
-    email:`${req.query.email}`,
-    class:`${req.query.class}`,
-    region:`${req.query.region}`,
-    password:`${req.query.password}`,
-    emailtoken:crypto.randomBytes(64).toString('hex')
-  })
-try {
- read.save()
- var main=true
- res.redirect("/social")
-}
-catch (e) {
- console.log("error occured")
-}
-
-})
+ 
 
 
 app.get("/",(req,res)=>[
